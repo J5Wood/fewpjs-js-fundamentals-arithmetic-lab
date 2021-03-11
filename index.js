138a55900ccd602640a59b39604b2f4106e2e451
Jeremy Wood <js.wood206@gmail.com>
2021-03-10 21:06:26 -0800

    commit tree 6a49d6d7549cd1d770a4b7d8714b8789649db74b

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 364;
+const num4 = 45;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max( 2, 5, 7, 16, 20, 1, 15, 3, 8)
